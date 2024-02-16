ae68ad0d49db7c58976061116d33b9de4e873f70
nathan <r4sl3xgod@gmail.com>
2024-02-17 01:22:48 +0700

    Исправление редактирования

--- kaka3/src/main.js
+++ kaka3/src/main.js
@@ -9,12 +9,21 @@ new Vue({
         newTaskDescription: '',
         newTaskDeadline: '',
         editingTask: null,
-        editedTask: { title: '', description: '', deadline: '' } // Объект для временного хранения отредактированных данных
+        editedTask: { title: '', description: '', deadline: '' }
     },
     methods: {
-        editTaskForm(index) {
-            this.editingTask = index;
-            const task = this.plannedTasks[index] || this.inProgressTasks[index] || this.testingTasks[index];
+        editTaskForm(column, index) {
+            this.editingTask = { column, index };
+            let task;
+            if (column === 'planned') {
+                task = this.plannedTasks[index];
+            } else if (column === 'inProgress') {
+                task = this.inProgressTasks[index];
+            } else if (column === 'testing') {
+                task = this.testingTasks[index];
+            } else if (column === 'completed') {
+                task = this.completedTasks[index];
+            }
             this.editedTask = {
                 title: task.title,
                 description: task.description,
@@ -23,16 +32,17 @@ new Vue({
         },
 
         saveEditedTask() {
-            let task;
             if (this.editingTask !== null) {
-                if (this.editingTask < this.plannedTasks.length) {
-                    task = this.plannedTasks[this.editingTask];
-                } else if (this.editingTask < this.plannedTasks.length + this.inProgressTasks.length) {
-                    task = this.inProgressTasks[this.editingTask - this.plannedTasks.length];
-                } else if (this.editingTask < this.plannedTasks.length + this.inProgressTasks.length + this.testingTasks.length) {
-                    task = this.testingTasks[this.editingTask - this.plannedTasks.length - this.inProgressTasks.length];
-                } else {
-                    task = this.completedTasks[this.editingTask - this.plannedTasks.length - this.inProgressTasks.length - this.testingTasks.length];
+                let task;
+                const { column, index } = this.editingTask;
+                if (column === 'planned') {
+                    task = this.plannedTasks[index];
+                } else if (column === 'inProgress') {
+                    task = this.inProgressTasks[index];
+                } else if (column === 'testing') {
+                    task = this.testingTasks[index];
+                } else if (column === 'completed') {
+                    task = this.completedTasks[index];
                 }
 
                 task.title = this.editedTask.title;
@@ -42,6 +52,7 @@ new Vue({
             }
             this.editingTask = null;
         },
+
 
         cancelEditTask() {
             this.editingTask = null;
