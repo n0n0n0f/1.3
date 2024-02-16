05dcec74bcc63e6f44415784b504c9940d6d28bb
nathan <r4sl3xgod@gmail.com>
2024-02-17 00:37:14 +0700

    Метод редактирования задачи

--- kaka3/src/main.js
+++ kaka3/src/main.js
@@ -12,7 +12,15 @@ new Vue({
         editedTask: { title: '', description: '', deadline: '' } // Объект для временного хранения отредактированных данных
     },
     methods: {
-
+        editTaskForm(index) {
+            this.editingTask = index;
+            const task = this.plannedTasks[index] || this.inProgressTasks[index] || this.testingTasks[index];
+            this.editedTask = {
+                title: task.title,
+                description: task.description,
+                deadline: task.deadline
+            };
+        },
 
 
         addTask(column) {
