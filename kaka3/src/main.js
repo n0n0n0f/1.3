21a01d2bc88c9fae4ab70b35a2c813f570b7d81b
nathan <r4sl3xgod@gmail.com>
2024-02-17 00:38:00 +0700

    Метод удаления  задачи

--- kaka3/src/main.js
+++ kaka3/src/main.js
@@ -61,6 +61,62 @@ new Vue({
             }
             this.clearForm();
         },
+        deleteTask(column, index) {
+            if (column === 'planned') {
+                this.plannedTasks.splice(index, 1);
+            } else if (column === 'inProgress') {
+                this.inProgressTasks.splice(index, 1);
+            } else if (column === 'testing') {
+                this.testingTasks.splice(index, 1);
+            } else if (column === 'completed') {
+                this.completedTasks.splice(index, 1);
+            }
+        },
+        moveTask(task) {
+            const index = this.plannedTasks.indexOf(task);
+            if (index !== -1) {
+                this.plannedTasks.splice(index, 1);
+                this.inProgressTasks.push(task);
+                return;
+            }
 
+            const index1 = this.inProgressTasks.indexOf(task);
+            if (index1 !== -1) {
+                this.inProgressTasks.splice(index1, 1);
+                this.testingTasks.push(task);
+                return;
+            }
+
+            const index2 = this.testingTasks.indexOf(task);
+            if (index2 !== -1) {
+                this.testingTasks.splice(index2, 1);
+                const currentDate = new Date();
+                const taskDeadline = new Date(task.deadline);
+                task.deadlineNoPassed = currentDate > taskDeadline;
+                this.completedTasks.push(task);
+                return;
+            }
+        },
+
+        removeTask(task) {
+            const index4 = this.completedTasks.indexOf(task);
+            if (index4 !== -1) {
+                this.completedTasks.splice(index4, 1);
+                this.testingTasks.push(task);
+                return;
+            }
+            const index3 = this.testingTasks.indexOf(task);
+            if (index3 !== -1 && task.returnReason) {
+                this.testingTasks.splice(index3, 1);
+                this.inProgressTasks.push(task);
+                return;
+            }
+        },
+
+        clearForm() {
+            this.newTaskTitle = '';
+            this.newTaskDescription = '';
+            this.newTaskDeadline = '';
+        }
     }
 });
